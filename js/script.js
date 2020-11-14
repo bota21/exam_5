ebe68ec9388aed6b4dedd1fa9a3673769a38cb4b
botagoz <botagoz.aimkulova@gmail.com>
2020-11-14 17:44:24 +0600

    #2 Добавлен слайдер к пятому блоку

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(() => {
+  $('.reviews_wrappers').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    arrows: true,
+    variableWidth: true
+  });
+})
